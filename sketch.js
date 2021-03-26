1cc487a345b6e25fd2aab64351669eefdd2a8d5f
kshitijsahni <72915861+kshitijsahni@users.noreply.github.com>
2021-03-26 12:54:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database,pos
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  var pos1= database.ref('ballon/position');
+  pos1.on('value',fly);
+
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    move(-1,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    move(1,0)
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    move(0,-1);
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    move(1,0);
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function move(x,y){
+  database.ref('balloon/position').set({
+
+    x: pos.x + x,
+    y : pos.y + y
+  });
+}
+
+function fly(data){
+  pos=data.val();
+  balloon.x=pos.x;
+  balloon.y=pos.y;
+}
